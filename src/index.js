c5d3e3c22fc5cad9dfcb818b2b7a93d6f01a3b7f
Mik <m.shac86@gmail.com>
2022-03-30 20:08:37 +0300

    HW_32

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-// function intersection(set1: Set<number>, set2: Set<number>): number[] {
-//     // TODO write function returning array of common numbers between two sets
-//     // that is the numbers existing in both sets
-//     const ar1 = Array.from(set1);
-//     return ar1.filter(e => set2.has(e));
-// }
-// function sbtract(set1: Set<number>, set2: Set<number>): number[] {
-//     // TODO write function returning array of common numbers between two sets
-//     // that is the numbers existing in both sets
-//     const ar1 = Array.from(set1);
-//     return ar1.filter(e => !set2.has(e));
-// }
+function intersection(set1, set2) {
+    // TODO write function returning array of common numbers between two sets
+    // that is the numbers existing in both sets
+    const ar1 = Array.from(set1);
+    return ar1.filter(e => set2.has(e));
+}
+function sbtract(set1, set2) {
+    // TODO write function returning array of common numbers between two sets
+    // that is the numbers existing in both sets
+    const ar1 = Array.from(set1);
+    return ar1.filter(e => !set2.has(e));
+}
 function getSortedOccurrences(array) {
     //TODO
     //write function returning array of occurrences 
@@ -18,13 +18,12 @@ function getSortedOccurrences(array) {
     //example: the given array is ['lmn', 'ab', 'a', 'cd', 'lmn', 'cd', 'lmn']
     //result: [{str: 'lmn', count: 3}, {str: 'cd', count: 2}, {str: 'a', count:1}, {str: 'ab', count:1}]
     //implementation notes: to use Map<string, number>
-    let res = array.reduce((acc, e) => ((acc[e] ? acc[e] += 1 : acc[e] = 1), acc), {});
+    const res = array.reduce((acc, e) => ((acc[e] ? acc[e] += 1 : acc[e] = 1), acc), {});
     const arraySort = Object.entries(res).sort((e1, e2) => {
         const res = e2[1] - e1[1];
         return res === 0 ? e1[0].localeCompare(e2[0], 'en') : res;
     });
-    let mapStrings = new Map([]);
-    mapStrings = arraySort.map((e) => {
+    const mapStrings = arraySort.map((e) => {
         return {
             str: e[0],
             count: e[1]
@@ -32,5 +31,9 @@ function getSortedOccurrences(array) {
     });
     return mapStrings;
 }
+const set1 = new Set([1, 2, 3, 4]);
+const set2 = new Set([3, 4, 5, 6]);
 const array = ['lmn', 'ab', 'a', 'cd', 'lmn', 'cd', 'lmn'];
+console.log(intersection(set1, set2));
+console.log(sbtract(set1, set2));
 console.log(getSortedOccurrences(array));
