1abfe9614dec4210776812b21212395c08ee99b3
Mik <m.shac86@gmail.com>
2022-04-02 15:51:56 +0300

    HW_33

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,132 @@
-function intersection(set1: Set<number>, set2: Set<number>): number[] {
-    const ar: number[] = [];
-    set1.forEach(e => set2.has(e) && ar.push(e));
-    return ar;
+interface Shape {
+    draw(): void;
 }
-function subtract(set1: Set<number>, set2: Set<number>): number[] {
-    return [...set1].filter(e => !set2.has(e));
+class Point implements Shape {
+    static readonly minValue = -100;
+    static readonly maxValue = 100;
+    static checkValue(value: number) {
+        if (value < Point.minValue || value > Point.maxValue) {
+            throw `wrong value ${value}, should be in range [from ${Point.minValue} to ${Point.maxValue}]`
+        }
+    }
+    constructor(private _x: number, private _y: number){
+        Point.checkValue(_x);
+        Point.checkValue(_y);
+    }
+    get x() {
+        return this._x;
+    }
+    get y() {
+        return this._y
+    }
+    set x(value: number) {
+        Point.checkValue(value);
+        this._x = value;
+    }
+    set y(value: number) {
+        Point.checkValue(value);
+        this._y = value;
+    }
+    draw() {
+        console.log('.......Point.......');
+        console.log(`Point [x: ${this._x}, y: ${this._y}]`);
+        console.log('.'.repeat(20));
+    } 
 }
-type Occurrency = {
-    str: string;
-    count: number;
-}
-function getSortedOccurrences(array: string[]): Occurrency[] { 
-    const resMap = new Map<string, number>();
-    array.forEach(str => resMap.set(str, resMap.has(str) ? resMap.get(str) + 1 : 1));
-    const resAr: Occurrency[] = [];
-    resMap.forEach((vol, key) => resAr.push({str: key, count: vol}));
-    resAr.sort((a, b) => b.count - a.count || a.str.localeCompare(b.str, 'en'));
-    return resAr;
-}
+class Line extends Point {
+    constructor(x: number, y: number, private _point: Point) {
+        super(x, y);
+    }
+    get point() {
+        return this._point;
+    }
+    draw() {
+        console.log('-------Line-------');
+        super.draw();
+        this._point.draw();
+        console.log('-'.repeat(20));
 
-const set1 = new Set<number>([1, 2, 3, 4]);
-const set2 = new Set<number>([3, 4, 5, 6]);
-const array: string[] = ['lmn', 'ab', 'a', 'cd', 'lmn', 'cd', 'lmn'];
-console.log(intersection(set1, set2));
-console.log(subtract(set1, set2));
-console.log(getSortedOccurrences(array));
+    }
+}
+class Square extends Point {
+    constructor(x: number, y: number, private _width: number) {
+        super(x, y);
+    }
+    get width() {
+        return this._width;
+    }
+    draw() {
+        console.log('+++++++Square+++++++');
+        super.draw();
+        console.log(`width: ${this._width}`);
+        console.log('+'.repeat(20));
+    }
+}
+class Rectangle extends Square {
+    constructor(x: number, y: number, width: number, private _height: number) {
+        super(x, y, width);
+    }
+    get height() {
+        return this._height;
+    }
+    draw() {
+        console.log('=======Rectangle=======');
+        super.draw();
+        console.log(`height: ${this._height}`);
+        console.log('='.repeat(20));
+    }
+}
+class Canvas implements Shape {
+    constructor(private _shapes: Shape[]) {};
+    addShape(shape: Shape): number {
+        this._shapes.push(shape);
+        return this._shapes.length - 1;
+    }
+    removeShape(index: number): Shape {
+        const array = this._shapes.splice(index, 1);
+        return array[0];
+    }
+    sort(): void {
+        this._shapes.sort((a, b) => {
+            if (a instanceof Point && b instanceof Point) {
+                return a.x - b.x || b.y - a.y
+            }
+        });
+    }
+    removeIf(predicate: (shape: Shape) => boolean) {
+        this._shapes = this._shapes.filter(predicate);
+    }
+    draw() {
+        this._shapes.forEach(shape => shape.draw());
+    }
+}
+const shapes: Shape[] = [
+    new Line(3, 4, new Point(10, 10)),
+    new Square(2, 5, 10),
+    new Line(20, 30, new Point(3, 4)),
+    new Rectangle(10, 15, 20, 5)
+]
+const canvas = new Canvas(shapes);
+console.log();
+console.log('//////////////////////////////Draw Shapes//////////////////////////////');
+canvas.draw();
+console.log();
+console.log('//////////////////////////////Add Shape//////////////////////////////');
+let addedIndex = canvas.addShape(new Line(20, 35, new Point(30, 25)));
+canvas.draw();
+console.log(`>>>>>>>>>> Index of the added shape : [${addedIndex}]`);
+console.log();
+console.log('//////////////////////////////Remove Shape//////////////////////////////');
+let removedShape = canvas.removeShape(3);
+canvas.draw();
+console.log(`>>>>>>>>>> Removed shape : ${removedShape.constructor.name}`);
+removedShape.draw();
+console.log();
+console.log('//////////////////////////////Sort Shapes//////////////////////////////');
+canvas.sort();
+canvas.draw();
+console.log();
+console.log('//////////////////////////////Remove IF//////////////////////////////');
+canvas.removeIf(shape => 
+    shape.constructor.name !== 'Line' || shape instanceof Line && shape.x > shape.point.x);
+canvas.draw();
